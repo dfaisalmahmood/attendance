b829cdfd52cf37a389f1bcd1427bb53d70e10250
ArisaftechFaisal <faisal.arisaftech@gmail.com>
2019-12-20 16:20:39 +0600

    changed run.bat

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ python manage.py migrate --run-syncdb
 REM python manage.py create_superuser_with_pass --username root --password 1234 --email blank@email.com --preserve --no-input
 python manage.py create_superuser_with_pass --username root --password 1234 --email blank@email.com --phone 09091451837 --first_name Admin --last_name Developer --preserve --no-input
 REM python manage.py collectstatic --no-input
-python manage.py runserver
+python manage.py runserver 7000
