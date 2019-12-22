976f62067738b77f95b8f6fc6b66dcebdf1809ad
Faisal Mahmood <faisal.arisaftech@gmail.com>
2019-12-23 00:31:13 +0600

    fixed requirements.txt

--- attendance/settings/base.py
+++ attendance/settings/base.py
@@ -31,8 +31,8 @@ DJOSER = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSIONS_CLASSES': (
